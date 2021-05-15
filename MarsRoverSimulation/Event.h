ff6ffcbcc9b006891f27ed7a6a2edc789b4c9190
Ahmed M. Gamal <amgamal200111@gmail.com>
2021-05-15 07:56:20 +0200

    event class

---
+++ MarsRoverSimulation/Event.h
@@ -0,0 +1,45 @@
+#pragma once
+class Event {
+private:
+	char eventType;
+	char missionType;
+	int eventDate;
+	int ID;
+	int targetLoc;
+	int misssionDur;
+	int sig;
+public:
+	Event(char ev, char typ, int ed, int id, int tloc, int mdur, int s); //If event is formulation
+	Event(char ev, int ed, int id); //If event is cancellation or promotion
+
+	//GETTERS
+	char getET() { return eventType; }
+	char getTYP() { return missionType; }
+	int getED() { return eventDate; }
+	int getID() { return ID; }
+	int getTLOC() { return targetLoc; }
+	int getMDUR() { return misssionDur; }
+	int getSIG() { return sig; }
+};
+
+Event::Event(char ev, char typ, int ed, int id, int tloc, int mdur, int s) {
+	eventType = ev;
+	missionType = typ;
+	eventDate = ed;
+	ID = id;
+	targetLoc = tloc;
+	misssionDur = mdur;
+	sig = s;
+}
+
+Event::Event(char ev, int ed, int id) {
+	eventType = ev;
+	eventDate = ed;
+	ID = id;
+
+	missionType = '.';
+	targetLoc = -1;
+	misssionDur = -1;
+	sig = -1;
+}
+
