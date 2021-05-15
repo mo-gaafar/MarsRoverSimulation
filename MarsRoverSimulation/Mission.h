b5a9d7a9ef057d1251750cc717c1eef5cc347717
Ahmed M. Gamal <amgamal200111@gmail.com>
2021-05-15 08:04:17 +0200

    mission class

---
+++ MarsRoverSimulation/Mission.h
@@ -0,0 +1,26 @@
+#pragma once
+class Mission {
+private:
+	int formulationDay;
+	char missionType;
+	int targetLoc;
+	int missionDur;
+	int sig;
+public:
+	Mission(int f, char mt, int tl, int md, int s); //Initialization
+
+	//GETTERS
+	int getFD() { return formulationDay; }
+	char getTYP() { return missionType; }
+	int getTLOC() { return targetLoc; }
+	int getMDUR() { return missionDur; }
+	int getSIG() { return sig; }
+};
+
+Mission::Mission(int f, char mt, int tl, int md, int s) {
+	formulationDay = f;
+	missionType = mt;
+	targetLoc = tl;
+	missionDur = md;
+	sig = s;
+}
