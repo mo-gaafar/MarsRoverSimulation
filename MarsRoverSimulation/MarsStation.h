21a8d7f05f2f85af8587c9470a36ef0a3af1fccf
Ahmed M. Gamal <amgamal200111@gmail.com>
2021-05-30 10:58:20 +0200

    Initialize function

--- MarsRoverSimulation/MarsStation.h
+++ MarsRoverSimulation/MarsStation.h
@@ -17,4 +17,12 @@ public:
 	MarsStation() {
 		day = 0;
 	}
+
+	void Initialize(char* Arr1, char* Arr2, int* Arr3, int* Arr4, int* Arr5, int* Arr6, int* Arr7, int size) {
+		ArrQueue<Event> EventList(size);
+		for (int i = 0; i < size; i++) {
+			Event E(Arr1[i], Arr2[i], Arr3[i], Arr4[i], Arr5[i], Arr6[i], Arr7[i]);
+			EventList.enqueue(E);
+		}
+	}
 };
