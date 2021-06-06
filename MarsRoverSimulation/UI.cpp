897b618561c1e55e96d9fb5a4e2894fd36aed1bc
batman_201288@yahoo.com <batman_201288@yahoo.com>
2021-06-06 05:06:20 +0200

    edited

--- MarsRoverSimulation/UI.cpp
+++ MarsRoverSimulation/UI.cpp
@@ -1,31 +1,47 @@
-#include"UI.h"
+#include "UI.h"
 void UI::read() {
 	ifstream input;
-	input.open("ay 7aga.txt");
+	input.open("Inputfile.txt");
 	if (input.fail()) {
 		cerr << "error opening file" << endl;
 		exit(1);
 	}
-	input >>  polrrovercount>> emerrovercount;
-	marsstaion->setemercount(emerrovercount);
-	marsstaion->setpolrcount(polrrovercount);
-	input >> polrspeed;
-	input >> emerspeed;	
-	input >> checkcount >> polrcheck >> emercheck;
-	createRovers();
-	input >> noevents;
-	marsstaion->seteventcount(noevents);
-	for (int i = 0; i < noevents; i++) {
-		input >> eventtype;
-		if (eventtype == 'F') {
-			input >> missiontype >> eventDay >> ID >> location >> duration >> signifiance;
-			marsstaion->createvent(missiontype, eventDay, ID,  location,  duration,  signifiance)
+	//LINE 1
+	input >> PolarRoverCount >> EmRoverCount;
+	//LINE 2
+	input >> PolarSpeed >> EmSpeed;
+	//LINE 3
+	input >> CheckCount >> PolarCheck >> EmCheck;
+	//line 4
+	input >> NumberOFEvents;
+	EventType = new char[NumberOFEvents];//dynamic array
+	MissionType = new char[NumberOFEvents];//dynamic array
+	EventDay = new int[NumberOFEvents];//dynamic array
+	ID = new int[NumberOFEvents];//dynamic array
+	location = new int[NumberOFEvents];//dynamic array
+	Duration = new int[NumberOFEvents];//dynamic array
+	Signifiance = new int[NumberOFEvents];//dynamic array
+	for (int i = 0; i < NumberOFEvents; i++) {//dynamic array
+		input >> EventType[i];
+		if (EventType[i] == 'F') {
+			input >> MissionType[i];
+			input >> EventDay[i];
+			input >> ID[i];
+			input >> location[i];
+			input >> Duration[i];
+			input >> Signifiance[i];
 		}
-		else if (eventtype == 'x') {
-			input>>  eventDay >> ID;
-			 marsstaion->createCancelEvent(eventDay, ID);
+		else {
+			MissionType[i] = 'x';
+			EventDay[i] = -1;
+			ID[i] = -1;
+			location[i] = -1;
+			Duration[i] = -1;
+			Signifiance[i] = -1;
 		}
-		
+
+
 	}
 	input.close();
+
 }
--- MarsRoverSimulation/UI.h
+++ MarsRoverSimulation/UI.h
@@ -1,19 +1,66 @@
 #pragma once
-#include"MarsStation.h"
+//#include"MarsStation.h"
+#include<iostream>
 #include<fstream>
+using namespace std;
 class UI {
-	MarsStation* marsstaion;
-	int emerrovercount, polrrovercount;
-	int emerspeed, polrspeed;
-	int emercheck, polrcheck, checkcount;
-	int noevents;
-	char eventtype, missiontype;
-	int eventDay, ID, location, duration, signifiance;
+	//MarsStation* marsstaion;
+	int EmRoverCount, PolarRoverCount;
+	int EmSpeed, PolarSpeed;
+	int EmCheck, PolarCheck, CheckCount;
+	int NumberOFEvents;
+	char* EventType, * MissionType;
+	int* EventDay, * ID, * location, * Duration, * Signifiance;
 
-	void read();
+
 	void createRovers();
 
-
-
-
+public:
+	void read();
+	int getEmRoverCount() {
+		return EmRoverCount;
+	}
+	int getPolarRoverCount() {
+		return PolarRoverCount;
+	}
+	int getEmSpeed() {
+		return EmSpeed;
+	}
+	int getPolarSpeed() {
+		return PolarSpeed;
+	}
+	int getEmCheck() {
+		return EmCheck;
+	}
+	int getPolarCheck() {
+		return PolarCheck;
+	}
+	int getCheckCount()
+	{
+		return CheckCount;
+	}
+	int getNumberOFEvents() {
+		return NumberOFEvents;
+	}
+	int* getID() {
+		return ID;
+	}
+	char* getMissionType() {
+		return MissionType;
+	}
+	char* getEventType() {
+		return EventType;
+	}
+	int* getEventDay() {
+		return EventDay;
+	}
+	int* getlocation() {
+		return location;
+	}
+	int* getDuration() {
+		return Duration;
+	}
+	int* getSignifiance() {
+		return Signifiance;
+	}
 };
